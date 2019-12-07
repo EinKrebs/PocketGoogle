c686cc3eea1c99f54a10d8e1f082c8b20929f109
Yury Frolov <yura200253@gmail.com>
2019-12-07 21:20:57 +0500

    Removed extra using`s.

--- Indexer.cs
+++ Indexer.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
-using System.Windows.Forms.VisualStyles;
 using NUnit.Framework;
 
 namespace PocketGoogle
